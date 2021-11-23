66dbe7c31e8b084868683c41e695268d9b4a18b5
Candinho Luiz Dalla Brida Junior <candinholuiz@gmail.com>
2021-11-23 11:06:14 -0300

    criar a estrutura inicial do jogo

--- jogo.js
+++ jogo.js
@@ -1,7 +1,111 @@
-console.log('Flappy Bird');
+console.log("[Candinho Jr] Flappy Bird");
 
 const sprites = new Image();
-sprites.src = './sprites.png';
+sprites.src = "./sprites.png";
 
-const canvas = document.querySelector('canvas');
-const contexto = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const contexto = canvas.getContext("2d");
+
+const chao = {
+  spriteX: 0, // Sprite X position
+  spriteY: 610, // Sprite Y position
+  largura: 224, // Largura - Width: Tamanho do recorte
+  altura: 112, // Altura - Height: Tamanho do recorte
+  x: 0, // canva X position
+  y: canvas.height - 112, // canva Y position
+  desenha() {
+    contexto.drawImage(
+      sprites, // Arquivo com as imagens
+      chao.spriteX,
+      chao.spriteY,
+      chao.largura,
+      chao.altura,
+      chao.x,
+      chao.y,
+      chao.largura,
+      chao.altura
+    );
+
+    contexto.drawImage(
+      sprites,
+      chao.spriteX,
+      chao.spriteY,
+      chao.largura,
+      chao.altura,
+      chao.x + chao.largura,
+      chao.y,
+      chao.largura,
+      chao.altura
+    );
+  },
+};
+
+const planoDeFundo = {
+  spriteX: 390,
+  spriteY: 0,
+  largura: 275,
+  altura: 204,
+  x: 0,
+  y: canvas.height - 204,
+  desenha() {
+    contexto.fillStyle = "#70c5ce";
+    contexto.fillRect(0, 0, canvas.width, canvas.height);
+
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX,
+      planoDeFundo.spriteY,
+      planoDeFundo.largura,
+      planoDeFundo.altura,
+      planoDeFundo.x,
+      planoDeFundo.y,
+      planoDeFundo.largura,
+      planoDeFundo.altura
+    );
+
+    contexto.drawImage(
+      sprites,
+      planoDeFundo.spriteX,
+      planoDeFundo.spriteY,
+      planoDeFundo.largura,
+      planoDeFundo.altura,
+      planoDeFundo.x + planoDeFundo.largura,
+      planoDeFundo.y,
+      planoDeFundo.largura,
+      planoDeFundo.altura
+    );
+  },
+};
+
+const flappyBird = {
+  spriteX: 0,
+  spriteX: 0,
+  spriteY: 0,
+  largura: 33,
+  altura: 24,
+  canvaX: 10,
+  y: 50,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      flappyBird.spriteX,
+      flappyBird.spriteY,
+      flappyBird.largura,
+      flappyBird.altura,
+      flappyBird.canvaX,
+      flappyBird.y,
+      flappyBird.largura,
+      flappyBird.altura
+    );
+  },
+};
+
+function loop() {
+  planoDeFundo.desenha();
+  chao.desenha();
+  flappyBird.desenha();
+
+  requestAnimationFrame(loop);
+}
+
+loop();
